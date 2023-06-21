4146259ab194bc3c75c0a8d905c5760586cc0952
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-21 20:16:20 +0300

    New Assignmnet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let submit = document.forms["create-task-form"]["submit"];
+let text =document.forms["create-task-form"]["new-task-description"];
+let list = document.getElementById("tasks")
+let button = document.createElement('button')
+   button.textContent="del"
+ 
+
+submit.addEventListener("click", (event)=>{
+    if(text.value){
+     let newList =document.createElement("li");
+     newList.textContent= text.value;
+     list.append(newList);
+      newList.append(button);
+      text.value="";
+      event.preventDefault()
+    }
+  })
+
+
+  button.addEventListener("click", (e)=>{
+   e.target.parentNode.remove()
+  })
+   
 });
+
+
